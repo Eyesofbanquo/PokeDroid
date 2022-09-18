ef69d95d19e300c6b2d93ac4e70c5fa1868cec2a
Markim Shaw <markimshaw@MBP-Intel-16.attlocal.net>
2022-09-18 13:41:13 -0500

    InfoView fully created

--- app/src/main/java/com/example/hilttut/InfoView.kt
+++ app/src/main/java/com/example/hilttut/InfoView.kt
@@ -4,13 +4,19 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.TextView
 
 class InfoView @JvmOverloads constructor(context: Context,
                                          attrs: AttributeSet? = null,
                                          defStyleAtr: Int = 0) :
      LinearLayout(context, attrs, defStyleAtr) {
 
+    lateinit var titleText: TextView
+    lateinit var subtitleText: TextView
+
     init {
         inflate(context, R.layout.info_view, this)
+        titleText = findViewById(R.id.title)
+        subtitleText = findViewById(R.id.subtitle)
     }
 }
