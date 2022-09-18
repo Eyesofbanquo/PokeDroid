2c6af095b319359607e8a842aa3a154a88f70127
Markim Shaw <markimshaw@MBP-Intel-16.attlocal.net>
2022-09-17 21:55:45 -0500

    Setup menu bar

--- app/src/main/java/com/example/hilttut/activities/MainActivity.kt
+++ app/src/main/java/com/example/hilttut/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.hilttut.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.Button
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -34,5 +35,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.layout_menu, menu)
+        return true
     }
 }
