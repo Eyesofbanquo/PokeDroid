e92abb899df59b95094e236d91063ee3511fe31f
Markim Shaw <markimshaw@MBP-Intel-16.attlocal.net>
2022-09-19 21:34:08 -0500

    Add endpoint to service

--- app/src/main/java/com/example/hilttut/network/PokemonService.kt
+++ app/src/main/java/com/example/hilttut/network/PokemonService.kt
@@ -1,5 +1,6 @@
 package com.example.hilttut.network
 
+import com.example.hilttut.model.PokeStats
 import com.example.hilttut.network.responses.AllPokemonResponse
 import com.example.hilttut.network.responses.PokemonFormResponse
 import retrofit2.Response
@@ -11,6 +12,8 @@ interface PokemonService {
     suspend fun getPokemon(): Response<AllPokemonResponse>
     @GET("/api/v2/pokemon-form/{id}")
     suspend fun getSpecificPokemonForm(@Path("id") id: String): Response<PokemonFormResponse>
+    @GET("/api/v2/{pokemon}")
+    suspend fun getSpecificPokemon(@Path("pokemon") pokemon: String): Response<PokeStats>
 }
 
 
