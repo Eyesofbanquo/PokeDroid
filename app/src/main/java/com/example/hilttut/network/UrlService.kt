2351a1442625483e9d2d00f670bc5571622af72d
Markim Shaw <markimshaw@MBP-Intel-16.attlocal.net>
2022-09-24 12:51:00 -0500

    Create GenericApiFactory

--- app/src/main/java/com/example/hilttut/network/UrlService.kt
+++ app/src/main/java/com/example/hilttut/network/UrlService.kt
@@ -1,4 +1,11 @@
 package com.example.hilttut.network
 
-class UrlService {
+import com.example.hilttut.network.responses.AllPokemonResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Url
+
+interface UrlService {
+    @GET
+    suspend fun loadUrl(@Url urlString: String): Response<AllPokemonResponse>
 }
---
+++ app/src/main/java/com/example/hilttut/network/apis/GenericApiFactory.kt
@@ -0,0 +1,26 @@
+package com.example.hilttut.network.apis
+
+import com.example.hilttut.network.UrlService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+@Module
+@InstallIn(ActivityComponent::class)
+object GenericApiFactory {
+
+    private fun getInstance(): Retrofit {
+        return Retrofit.Builder()
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    fun makeGenericService(): UrlService {
+        getInstance()
+            .create(UrlService::class.java)
+    }
+}
